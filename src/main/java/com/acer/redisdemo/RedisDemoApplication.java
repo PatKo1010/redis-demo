ac185d6e74d1a30a230a7b01b60f1f7b4f7eec12
Patrick.Ko <Patrick.Ko@acer.com>
2022-10-31 11:36:40 +0800

    setting up redis config
    Signed-off-by: Patrick.Ko <Patrick.Ko@acer.com>

--- src/main/java/com/acer/redisdemo/RedisDemoApplication.java
+++ src/main/java/com/acer/redisdemo/RedisDemoApplication.java
@@ -2,8 +2,10 @@ package com.acer.redisdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(value = {com.acer.redisdemo.configuration.RedisConfig.class})
 public class RedisDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/acer/redisdemo/controller/HelloController.java
+++ src/main/java/com/acer/redisdemo/controller/HelloController.java
@@ -1,14 +1,25 @@
 package com.acer.redisdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HelloController {
+	@Autowired
+	RedisTemplate redisTemplate; 
 
-	@GetMapping("/")
+	@GetMapping("/index")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		Boolean hasKey = redisTemplate.hasKey("1");
+		return "Hello SpringBoot " + hasKey;
 	}
+	
+	
+
+
 
 }
